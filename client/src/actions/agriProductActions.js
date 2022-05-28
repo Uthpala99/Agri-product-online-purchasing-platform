576445fc14a05b11f6d1bee84aadf573e77d3f2e
aroshana <rasitharoshana0215@gmail.com>
2022-05-29 01:57:59 +0530

    wso2 updates in front end

--- client/src/actions/agriProductActions.js
+++ client/src/actions/agriProductActions.js
@@ -1,81 +1,101 @@
-import axios from "axios" ;
+import axios from "axios";
 
-export const getAllAgriProducts = () =>  async dispatch =>{
-    dispatch({type:'GET_AGRIPRODUCTS_REQUEST'})
+export const getAllAgriProducts = () => async (dispatch) => {
+  dispatch({ type: "GET_AGRIPRODUCTS_REQUEST" });
 
-    try {
-        const response = await axios.get('http://localhost:8000/api/agriproducts/getallagriproducts')
-        console.log(response)
-        dispatch({type:'GET_AGRIPRODUCTS_SUCCESS' , payload : response.data})
-    } catch (error) {
-        dispatch({type:'GET_AGRIPRODUCTS_FAILED' , payload : error })
+  try {
+    const response = await axios.get(
+      "http://localhost:8000/api/agriproducts/getallagriproducts"
+    );
+    console.log(response);
+    dispatch({ type: "GET_AGRIPRODUCTS_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({ type: "GET_AGRIPRODUCTS_FAILED", payload: error });
+  }
+};
+
+export const getAgriProductById = (agriproductid) => async (dispatch) => {
+  dispatch({ type: "GET_AGRIPRODUCTBYID_REQUEST" });
+
+  try {
+    const response = await axios.post(
+      "http://localhost:8000/api/agriproducts/getagriproductbyid",
+      { agriproductid }
+    );
+    console.log(response);
+    dispatch({ type: "GET_AGRIPRODUCTBYID_SUCCESS", payload: response.data });
+  } catch (error) {
+    dispatch({ type: "GET_AGRIPRODUCTBYID_FAILED", payload: error });
+  }
+};
+
+export const filterAgriProducts = (searchkey, category) => async (dispatch) => {
+  var filteredAgriProducts;
+  dispatch({ type: "GET_AGRIPRODUCTS_REQUEST" });
+
+  try {
+    const response = await axios.get(
+      "http://localhost:8000/api/agriproducts/getallagriproducts"
+    );
+    filteredAgriProducts = response.data.filter((agriProduct) =>
+      agriProduct.name.toLowerCase().includes(searchkey)
+    );
+    if (category != "all") {
+      filteredAgriProducts = response.data.filter(
+        (agriProduct) => agriProduct.category.toLowerCase() == category
+      );
     }
-} 
+    console.log(response);
+    dispatch({
+      type: "GET_AGRIPRODUCTS_SUCCESS",
+      payload: filteredAgriProducts,
+    });
+  } catch (error) {
+    dispatch({ type: "GET_AGRIPRODUCTS_FAILED", payload: error });
+  }
+};
 
+export const addAgriProduct = (agriProduct) => async (dispatch) => {
+  dispatch({ type: "ADD_AGRIPRODUCT_REQUEST" });
+  try {
+    const response = await axios.post(
+      "http://localhost:8290/agriProduct/addProduct",
+      { agriProduct }
+    );
+    console.log(response);
+    dispatch({ type: "ADD_AGRIPRODUCT_SUCCESS" });
+    window.location.href = "/admin/agriproductslist";
+  } catch (error) {
+    dispatch({ type: "ADD_AGRIPRODUCT_FAILED", payload: error });
+  }
+};
 
-export const getAgriProductById = (agriproductid) =>  async dispatch =>{
-    dispatch({type:'GET_AGRIPRODUCTBYID_REQUEST'})
+export const editAgriProduct = (editedagriProduct) => async (dispatch) => {
+  dispatch({ type: "EDIT_AGRIPRODUCT_REQUEST" });
+  try {
+    const response = await axios.post(
+      "http://localhost:8290/agriProduct/editProduct",
+      { editedagriProduct }
+    );
+    console.log(response);
+    dispatch({ type: "EDIT_AGRIPRODUCT_SUCCESS" });
+    window.location.href = "/admin/agriproductslist";
+  } catch (error) {
+    dispatch({ type: "EDIT_AGRIPRODUCT_FAILED", payload: error });
+  }
+};
 
-    try {
-        const response = await axios.post('http://localhost:8000/api/agriproducts/getagriproductbyid' , {agriproductid})
-        console.log(response)
-        dispatch({type:'GET_AGRIPRODUCTBYID_SUCCESS' , payload : response.data})
-    } catch (error) {
-        dispatch({type:'GET_AGRIPRODUCTBYID_FAILED' , payload : error })
-    }
-}
-
-export const filterAgriProducts = (searchkey , category) =>  async dispatch =>{
-    var filteredAgriProducts ;
-    dispatch({type:'GET_AGRIPRODUCTS_REQUEST'})
-
-    try {
-        const response = await axios.get('http://localhost:8000/api/agriproducts/getallagriproducts')
-        filteredAgriProducts = response.data.filter(agriProduct=>agriProduct.name.toLowerCase().includes(searchkey))
-        if(category!='all'){
-            filteredAgriProducts = response.data.filter(agriProduct=>agriProduct.category.toLowerCase()==category)
-        }
-        console.log(response)
-        dispatch({type:'GET_AGRIPRODUCTS_SUCCESS' , payload : filteredAgriProducts})
-    } catch (error) {
-        dispatch({type:'GET_AGRIPRODUCTS_FAILED' , payload : error })
-    }
-} 
-
-export const addAgriProduct=(agriProduct)=> async dispatch=>{
-    dispatch({type:'ADD_AGRIPRODUCT_REQUEST'})
-    try {
-        const response = await axios.post('http://localhost:8000/api/agriproducts/addagriproduct' , {agriProduct})
-        console.log(response);
-        dispatch({type:'ADD_AGRIPRODUCT_SUCCESS'})
-        window.location.href="/admin/agriproductslist"
-    } catch (error) {
-        dispatch({type:'ADD_AGRIPRODUCT_FAILED' , payload : error})
-    }
-}
-
-
-export const editAgriProduct=(editedagriProduct)=> async dispatch=>{
-    dispatch({type:'EDIT_AGRIPRODUCT_REQUEST'})
-    try {
-        const response = await axios.post('http://localhost:8000/api/agriproducts/editagriproduct' , {editedagriProduct})
-        console.log(response);
-        dispatch({type:'EDIT_AGRIPRODUCT_SUCCESS'})
-        window.location.href="/admin/agriproductslist"
-    } catch (error) {
-        dispatch({type:'EDIT_AGRIPRODUCT_FAILED' , payload : error})
-    }
-}
-
-export const deleteAgriProduct =(agriproductid)=>async dispatch=>{
-    try {
-        const response = await axios.post('http://localhost:8000/api/agriproducts/deleteagriproduct' , {agriproductid})
-        alert('Agri Product Deleted Successfully')
-        console.log(response);
-        window.location.reload();
-    } catch (error) {
-        alert('Something Went Wrong ')
-        console.log(error);
-        
-    }
-}
+export const deleteAgriProduct = (agriproductid) => async (dispatch) => {
+  try {
+    const response = await axios.post(
+      "http://localhost:8290/agriProduct/deleteProduct",
+      { agriproductid }
+    );
+    alert("Agri Product Deleted Successfully");
+    console.log(response);
+    window.location.reload();
+  } catch (error) {
+    alert("Something Went Wrong ");
+    console.log(error);
+  }
+};
--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -1,38 +1,38 @@
-import axios from "axios"
+import axios from "axios";
 
+export const registerUser = (user) => async (dispatch) => {
+  dispatch({ type: "USER_REGISTER_REQUEST" });
 
-export const registerUser=(user)=>async dispatch=>{
+  try {
+    const response = await axios.post(
+      "http://localhost:8290/users/register",
+      user
+    );
+    console.log(response);
+    dispatch({ type: "USER_REGISTER_SUCCESS" });
+  } catch (error) {
+    dispatch({ type: "USER_REGISTER_FAILED", payload: error });
+  }
+};
 
-    dispatch({type:'USER_REGISTER_REQUEST'})
+export const loginUser = (user) => async (dispatch) => {
+  dispatch({ type: "USER_LOGIN_REQUEST" });
 
-    try {
-        const response = await axios.post('http://localhost:8002/api/users/register' , user )
-        console.log(response)
-        dispatch({type:'USER_REGISTER_SUCCESS'})
-    } catch (error) {
-        dispatch({type:'USER_REGISTER_FAILED' , payload : error})
-        
-    }
-}
+  try {
+    const response = await axios.post(
+      "http://localhost:8290/users/login",
+      user
+    );
+    console.log(response);
+    dispatch({ type: "USER_LOGIN_SUCCESS", payload: response.data });
+    localStorage.setItem("currentUser", JSON.stringify(response.data));
+    window.location.href = "/";
+  } catch (error) {
+    dispatch({ type: "USER_LOGIN_FAILED", payload: error });
+  }
+};
 
-
-export const loginUser=(user)=>async dispatch=>{
-
-    dispatch({type:'USER_LOGIN_REQUEST'})
-
-    try { 
-        const response = await axios.post('http://localhost:8002/api/users/login' , user )
-        console.log(response)
-        dispatch({type:'USER_LOGIN_SUCCESS' , payload : response.data })
-        localStorage.setItem('currentUser' , JSON.stringify(response.data)) 
-        window.location.href='/'
-    } catch (error) {
-        dispatch({type:'USER_LOGIN_FAILED' , payload : error})
-        
-    }
-}
-
-export const logoutUser =()=>dispatch=>{
-    localStorage.removeItem('currentUser')
-    window.location.href='/login'
-}
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("currentUser");
+  window.location.href = "/login";
+};
